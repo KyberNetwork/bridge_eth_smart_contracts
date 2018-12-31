a8c6650f7a1f26dc6c53f92f0b5685e644164c8c
Tal Baneth <tal@kyber.network>
2018-12-31 12:21:28 +0200

    Parsing header, before order

--- contracts/Relay.sol
+++ contracts/Relay.sol
@@ -6,50 +6,50 @@ contract Relay {
     // chunk 0
     uint constant TIMESTAMP_BITS  = 32;
     uint constant TIMESTAMP_MASK = 2 ** TIMESTAMP_BITS - 1;
-    uint constant TIMESTAMP_OFFSET = 256 - TIMESTAMP_BITS;
+    uint constant TIMESTAMP_OFFSET = 256 - TIMESTAMP_BITS; // 224
 
     uint constant PRODUCER_BITS  = 64;
     uint constant PRODUCER_MASK = 2 ** PRODUCER_BITS - 1;
-    uint constant PRODUCER_OFFSET = TIMESTAMP_OFFSET - PRODUCER_BITS;
+    uint constant PRODUCER_OFFSET = TIMESTAMP_OFFSET - PRODUCER_BITS; // 160
 
     uint constant CONFIRMED_BITS  = 16;
     uint constant CONFIRMED_MASK = 2 ** CONFIRMED_BITS - 1;
-    uint constant CONFIRMED_OFFSET = PRODUCER_OFFSET - CONFIRMED_BITS;
+    uint constant CONFIRMED_OFFSET = PRODUCER_OFFSET - CONFIRMED_BITS; // 144
 
-    uint constant PREVIOUS_0_BITS  = CONFIRMED_OFFSET;
+    uint constant PREVIOUS_0_BITS  = CONFIRMED_OFFSET; // 144
     uint constant PREVIOUS_0_MASK = 2 ** PREVIOUS_0_BITS - 1;
-    uint constant PREVIOUS_0_OFFSET = CONFIRMED_OFFSET - PREVIOUS_0_BITS;
+    uint constant PREVIOUS_0_OFFSET = 0;
 
     // chunk 1
-    uint constant PREVIOUS_1_BITS  = 256 - PREVIOUS_0_BITS;
+    uint constant PREVIOUS_1_BITS  = 256 - PREVIOUS_0_BITS; // 112
     uint constant PREVIOUS_1_MASK = 2 ** PREVIOUS_1_BITS - 1;
-    uint constant PREVIOUS_1_OFFSET = 256 - PREVIOUS_1_BITS;
+    uint constant PREVIOUS_1_OFFSET = 256 - PREVIOUS_1_BITS; // 144
 
-    uint constant TX_MROOT_0_BITS  = 256 - PREVIOUS_1_BITS;
+    uint constant TX_MROOT_0_BITS  = 256 - PREVIOUS_1_BITS; // 144
     uint constant TX_MROOT_0_MASK = 2 ** TX_MROOT_0_BITS - 1;
-    uint constant TX_MROOT_0_OFFSET = PREVIOUS_1_OFFSET - TX_MROOT_0_BITS;
+    uint constant TX_MROOT_0_OFFSET = 0;
 
     // chunk 2
-    uint constant TX_MROOT_1_BITS  = 256 - TX_MROOT_0_BITS;
+    uint constant TX_MROOT_1_BITS  = 256 - TX_MROOT_0_BITS; // 112
     uint constant TX_MROOT_1_MASK = 2 ** TX_MROOT_1_BITS - 1;
-    uint constant TX_MROOT_1_OFFSET = 256 - TX_MROOT_1_BITS;
+    uint constant TX_MROOT_1_OFFSET = 256 - TX_MROOT_1_BITS; // 144
 
-    uint constant ACTION_MROOT_0_BITS  = 256 - TX_MROOT_1_BITS;
+    uint constant ACTION_MROOT_0_BITS  = 256 - TX_MROOT_1_BITS; // 144
     uint constant ACTION_MROOT_0_MASK = 2 ** ACTION_MROOT_0_BITS - 1;
-    uint constant ACTION_MROOT_0_OFFSET = TX_MROOT_1_OFFSET - ACTION_MROOT_0_BITS;  
+    uint constant ACTION_MROOT_0_OFFSET = 0; 
 
     // chunk 3
-    uint constant ACTION_MROOT_1_BITS  = 256 - ACTION_MROOT_0_BITS;
+    uint constant ACTION_MROOT_1_BITS  = 256 - ACTION_MROOT_0_BITS; // 112
     uint constant ACTION_MROOT_1_MASK = 2 ** ACTION_MROOT_1_BITS - 1;
-    uint constant ACTION_MROOT_1_OFFSET = 160 - ACTION_MROOT_1_BITS; // since reading it to a 20B buffer
+    uint constant ACTION_MROOT_1_OFFSET = 160 - ACTION_MROOT_1_BITS; // 48
 
     uint constant SCHEDULE_BITS  = 32;
     uint constant SCHEDULE_MASK = 2 ** SCHEDULE_BITS - 1;
-    uint constant SCHEDULE_OFFSET = ACTION_MROOT_1_OFFSET - SCHEDULE_BITS;
+    uint constant SCHEDULE_OFFSET = ACTION_MROOT_1_OFFSET - SCHEDULE_BITS; // 16
 
     uint constant HAVE_NEW_PRODUCERS_BITS  = 8;
     uint constant HAVE_NEW_PRODUCERS_MASK = 2 ** HAVE_NEW_PRODUCERS_BITS - 1;
-    uint constant HAVE_NEW_PRODUCERS_OFFSET = SCHEDULE_OFFSET - HAVE_NEW_PRODUCERS_BITS;
+    uint constant HAVE_NEW_PRODUCERS_OFFSET = SCHEDULE_OFFSET - HAVE_NEW_PRODUCERS_BITS; // 8
 
     function sliceBytes32(bytes memory bs, uint start) internal pure returns (uint)
     {
@@ -71,43 +71,116 @@ contract Relay {
             return x;
     }
 
+    function sliceBytes4(bytes memory bs, uint start) internal pure returns (uint32)
+    {
+        require(bs.length >= start + 4, "slicing out of range");
+        uint32 x;
+        assembly {
+            x := mload(add(bs, add(0x4, start)))
+        }
+            return x;
+    }
 
-    function parseHeader(bytes calldata blockHeader) external pure returns (uint8){
-        /* expected sizes 4, 8, 2, 32, 32, 32, 4, 1, 1 */
-        //https://ethereum.stackexchange.com/questions/42659/parse-arbitrary-bytes-input
+    function parseFixedFields0(bytes memory blockHeader)
+        internal
+        pure
+        returns (uint32 timestamp, uint64 producer, uint16 confirmed, uint previous, uint tx_mroot)
+    {
 
         uint chunk;
 
         chunk = sliceBytes32(blockHeader, 0);
-        uint32 timestamp = (uint32)((chunk >> TIMESTAMP_OFFSET) & TIMESTAMP_MASK);
-        uint64 producer = (uint64)((chunk >> PRODUCER_OFFSET) & PRODUCER_MASK);
-        uint16 confirmed = (uint16)((chunk >> CONFIRMED_OFFSET) & CONFIRMED_MASK);
+        timestamp = (uint32)((chunk >> TIMESTAMP_OFFSET) & TIMESTAMP_MASK);
+        producer = (uint64)((chunk >> PRODUCER_OFFSET) & PRODUCER_MASK);
+        confirmed = (uint16)((chunk >> CONFIRMED_OFFSET) & CONFIRMED_MASK);
         uint previous_0 = (uint)((chunk >> PREVIOUS_0_OFFSET) & PREVIOUS_0_MASK);
 
         chunk = sliceBytes32(blockHeader, 32);
         uint previous_1 = (uint)((chunk >> PREVIOUS_1_OFFSET) & PREVIOUS_1_MASK);
         uint tx_mroot_0 = (uint)((chunk >> TX_MROOT_0_OFFSET) & TX_MROOT_0_MASK);
-        
+
         chunk = sliceBytes32(blockHeader, 64);
         uint tx_mroot_1 = (uint)((chunk >> TX_MROOT_1_OFFSET) & TX_MROOT_1_MASK);
+
+        previous = (previous_0 << PREVIOUS_1_BITS) | previous_1; 
+        tx_mroot = (tx_mroot_0 << TX_MROOT_1_BITS) | tx_mroot_1;
+    }
+
+    function parseFixedFields1(bytes memory blockHeader)
+        internal
+        pure
+        returns (uint32 schedule, uint action_mroot, uint8 have_new_producers)
+    {
+        // TODO: remove duplication
+
+        uint chunk;
+        chunk = sliceBytes32(blockHeader, 64);
         uint action_mroot_0 = (uint)((chunk >> ACTION_MROOT_0_OFFSET) & ACTION_MROOT_0_MASK);
 
         // read only 20B, since not sure if optional fields are there
         uint chunk160 = sliceBytes20(blockHeader, 96);
         uint action_mroot_1 = (uint)((chunk160 >> ACTION_MROOT_1_OFFSET) & ACTION_MROOT_1_MASK);
-        uint32 schedule = (uint32)((chunk160 >> SCHEDULE_OFFSET) & SCHEDULE_MASK);
-        // new producers field is optional
-        uint8 have_new_producers = (uint8)((chunk160 >> HAVE_NEW_PRODUCERS_OFFSET) & HAVE_NEW_PRODUCERS_MASK);
+        schedule = (uint32)((chunk160 >> SCHEDULE_OFFSET) & SCHEDULE_MASK);
+        have_new_producers = (uint8)((chunk160 >> HAVE_NEW_PRODUCERS_OFFSET) & HAVE_NEW_PRODUCERS_MASK);
 
-        if(have_new_producers) {
-            // TBD..
+        action_mroot = (action_mroot_0 << ACTION_MROOT_1_BITS) | action_mroot_1;
+    }
+
+    function parseNonFixedFields(bytes memory blockHeader)
+        internal
+        pure
+        returns (uint32 version, uint8 amount, uint64[21] memory producerNames, bytes32[21] memory producerKeyHighChunk)
+    {
+        version = sliceBytes4(blockHeader, 96 + 19); // 4 bytes version
+        amount = (uint8)(blockHeader[96+19+4]);// 1 byte amount
+
+        uint offset = 120; 
+
+        for (uint i = 0; i < amount; i++) {
+            require(blockHeader.length >= offset + 8, "slicing out of range");
+            uint64 x;
+            assembly {x := mload(add(blockHeader, add(0x8, offset)))}
+            producerNames[i] = x;
+            offset = offset + 8;
+
+            offset = offset + 1; // skip 1 zeroed bytes
+            offset = offset + 1; // skip first byte of the key
+
+            require(blockHeader.length >= offset + 32, "slicing out of range");
+            bytes32 y;
+            assembly {y := mload(add(blockHeader, add(32, offset)))}
+            producerKeyHighChunk[i] = y; 
+            offset = offset + 32;
         }
+    } 
 
-        uint previous = (previous_0 << PREVIOUS_1_BITS) | previous_1; 
-        uint tx_mroot = (tx_mroot_0 << TX_MROOT_1_BITS) | tx_mroot_1;
-        uint action_mroot = (action_mroot_0 << ACTION_MROOT_1_BITS) | action_mroot_1;
+    function parseHeader(bytes calldata blockHeader)
+        external
+        pure
+        returns (
+            uint32 timestamp,
+            uint64 producer,
+            uint16 confirmed,
+            uint previous,
+            uint tx_mroot,
+            uint32 schedule,
+            uint action_mroot,
+            uint32 version,
+            uint8 amount,
+            uint64[21] memory producerNames,
+            bytes32[21] memory producerKeyHighChunk // TODO: this should be 33 bytes!!!
+        )
+    {
+        /* expected sizes 4, 8, 2, 32, 32, 32, 4, 1, 1 */
 
-        return have_new_producers;
+        (timestamp, producer, confirmed, previous, tx_mroot) = parseFixedFields0(blockHeader);
+        uint8 have_new_producers;
+        (schedule, action_mroot, have_new_producers) = parseFixedFields1(blockHeader);
+
+        if(have_new_producers != 0 ) {
+            
+            (version, amount, producerNames, producerKeyHighChunk) = parseNonFixedFields(blockHeader);
+        }
     }
 
     function verifyBlockSig(
--- test/relay.js
+++ test/relay.js
@@ -92,12 +92,22 @@ contract("Relay", async accounts => {
     })
     it("parse header", async () => {
         // this includes new producers:
-        headerRaw = "[ a4 98 76 47 00 00 00 00 00 ea 30 55 00 00 00 00 00 01 bc f2 f4 48 22 5d 09 96 85 f1 4d a7 68 03 02 89 26 af 04 d2 60 7e af cf 60 9c 26 5c 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 74 7d 10 3e 24 c9 6d eb 1b ee bc 13 eb 31 f7 c2 18 81 26 94 6c 86 77 df d1 69 1a f9 f9 c0 3a b1 00 00 00 00 01 17 00 00 00 02 00 00 00 00 00 ea 30 55 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 00 00 00 00 73 a2 c9 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 ]"
+        headerRaw = "[ 95 18 79 47 00 00 00 00 00 ea 30 55 00 00 00 00 00 01 bc f2 f4 48 22 5d 09 96 85 f1 4d a7 68 03 02 89 26 af 04 d2 60 7e af cf 60 9c 26 5c 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 74 7d 10 3e 24 c9 6d eb 1b ee bc 13 eb 31 f7 c2 18 81 26 94 6c 86 77 df d1 69 1a f9 f9 c0 3a b1 00 00 00 00 01 57 01 00 00 04 00 00 00 00 00 ea 30 55 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 00 00 00 00 73 a2 c9 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 00 00 00 80 49 af f1 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 00 00 00 80 69 a2 73 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 ]"
         header = "0x"+toBuffer(headerRaw).toString("hex")
         console.log("header", header)
 
         const relay = await Relay.new()
         const result = await relay.parseHeader(header);
-        console.log(result);
+        console.log("timestamp: ", result[0]);
+        console.log("producer: ", result[1]);
+        console.log("confirmed: ", result[2]);
+        console.log("previous: ", result[3]);
+        console.log("tx_mroot: ", result[4]);
+        console.log("schedule: ", result[5]);
+        console.log("action_mroot: ", result[6]);
+        console.log("version: ", result[7]);
+        console.log("amount: ", result[8]);
+        console.log("producerNames: \n", result[9]);
+        console.log("producerKeysHigh: \n", result[10]);
     });
 })

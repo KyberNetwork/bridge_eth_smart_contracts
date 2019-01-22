009b08bb7b2d95d8e8dacb5358214cba1d705298
Tal Baneth <tal@kyber.network>
2019-01-22 13:39:28 +0200

    contract parses and links 15 blocks with merkle

--- contracts/Relay.sol
+++ contracts/Relay.sol
@@ -1,206 +1,26 @@
 pragma solidity 0.5.0;
 
+import "./HeaderParser.sol";
 
-contract Relay {
 
-    uint constant TIMESTAMP_BYTES               = 4;
-    uint constant PRODUCER_BYTES                = 8;
-    uint constant CONFIRMED_BYTES               = 2;
-    uint constant PREVIOUS_BYTES                = 32;
-    uint constant TX_MROOT_BYTES                = 32;
-    uint constant ACTION_MROOT_BYTES            = 32;
-    uint constant SCHEDULE_BYTES                = 4;
-    uint constant HAVE_NEW_PRODUCERS_BYTES      = 1;
-    uint constant PRODUCERS_VERSION_BYTES       = 4;
-    uint constant PRODUCERS_NAME_BYTES          = 8;
-    uint constant PRODUCERS_AMOUNT_BYTES        = 1;
-    uint constant PRODUCERS_KEY_HIGH_BYTES      = 32;
-    uint constant OR_MASK =   0x8000000000000000000000000000000000000000000000000000000000000000;
-    uint constant AND_MASK =  0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+contract Relay is HeaderParser {
+
+    struct HeadersData {
+        bytes blockHeaders;
+        uint[] blockHeaderSizes;
+        bytes32[] blockMerkleHashs;
+        bytes32[] blockMerklePaths;
+        uint[] blockMerklePathSizes;
+        bytes32[] pendingScheduleHashs;
+        uint8[15] sigVs;
+        bytes32[15] sigRs;
+        bytes32[15] sigSs;
+        uint[15] claimedKeyIndices;
+    }
 
     bytes32[21] public pubKeysFirstParts;
     bytes32[21] public pubKeysSecondParts;
     uint public scheduleVersion;
-
-    function sliceBytes(bytes memory bs, uint start, uint size) internal pure returns (uint)
-    {
-        require(bs.length >= start + size, "slicing out of range");
-        uint x;
-        assembly {
-            x := mload(add(bs, add(size, start)))
-        }
-        return x;
-    }
-
-    function reverseBytes(uint32 input) internal pure returns (uint32 output)
-    {
-        output = input >> 24 & 0xff | input >> 8 & 0xff00 | input << 8 & 0xff0000 |  input << 24 & 0xff000000;
-    }
-
-    function parseFixedFields0(bytes memory blockHeader)
-        internal
-        pure
-        returns (uint32 ts, uint64 producer, uint16 confirmed, uint previous, uint tx_mroot)
-    {
-        uint offset = 0;
-
-        ts = reverseBytes((uint32)(sliceBytes(blockHeader, offset, TIMESTAMP_BYTES)));
-        offset = offset + TIMESTAMP_BYTES;
-
-        producer = (uint64)(sliceBytes(blockHeader, offset, PRODUCER_BYTES));
-        offset = offset + PRODUCER_BYTES;
-
-        confirmed = (uint16)(sliceBytes(blockHeader, offset, CONFIRMED_BYTES));
-        offset = offset + CONFIRMED_BYTES;
-
-        previous = (uint256)(sliceBytes(blockHeader, offset, PREVIOUS_BYTES));
-        offset = offset + PREVIOUS_BYTES;
-
-        tx_mroot = (uint256)(sliceBytes(blockHeader, offset, TX_MROOT_BYTES));
-        offset = offset + TX_MROOT_BYTES;
-    }
-
-    function parseFixedFields1(bytes memory blockHeader)
-        internal
-        pure
-        returns (uint32 schedule, uint action_mroot, uint8 have_new_producers)
-    {
-        uint offset = 78;
-
-        schedule = reverseBytes((uint32)(sliceBytes(blockHeader, offset, SCHEDULE_BYTES)));
-        offset = offset + SCHEDULE_BYTES;
-
-        action_mroot = (uint256)(sliceBytes(blockHeader, offset, ACTION_MROOT_BYTES));
-        offset = offset + ACTION_MROOT_BYTES;
-
-        have_new_producers = (uint8)(sliceBytes(blockHeader, offset, HAVE_NEW_PRODUCERS_BYTES));
-        offset = offset + HAVE_NEW_PRODUCERS_BYTES;
-    }
-
-    function parseNonFixedFields(bytes memory blockHeader)
-        internal
-        pure
-        returns (uint32 version, uint8 amount, uint64[21] memory producerNames, bytes32[21] memory producerKeyHighChunk)
-    {
-        uint offset = 115;
-
-        version = reverseBytes((uint32)(sliceBytes(blockHeader, offset, PRODUCERS_VERSION_BYTES)));
-        offset = offset + PRODUCERS_VERSION_BYTES;
-
-        amount = (uint8)(sliceBytes(blockHeader, offset, PRODUCERS_AMOUNT_BYTES));
-        offset = offset + PRODUCERS_AMOUNT_BYTES;
-
-        for (uint i = 0; i < amount; i++) {
-            producerNames[i] = (uint64)(sliceBytes(blockHeader, offset, PRODUCERS_NAME_BYTES));
-            offset = offset + PRODUCERS_NAME_BYTES;
-
-            offset = offset + 1; // skip 1 zeroed bytes
-            offset = offset + 1; // skip first byte of the key
-
-            producerKeyHighChunk[i] = (bytes32)(sliceBytes(blockHeader, offset, PRODUCERS_KEY_HIGH_BYTES));
-            offset = offset + PRODUCERS_KEY_HIGH_BYTES;
-        }
-    } 
-
-    function parseHeader(bytes calldata blockHeader)
-        external
-        pure
-        returns (
-            uint32 timestamp,
-            uint64 producer,
-            uint16 confirmed,
-            uint previous,
-            uint tx_mroot,
-            uint32 schedule,
-            uint action_mroot,
-            uint32 version,
-            uint8 amount,
-            uint64[21] memory producerNames,
-            bytes32[21] memory producerKeyHighChunk // TODO: this should be 33 bytes!!!
-        )
-    {
-        (timestamp, producer, confirmed, previous, tx_mroot) = parseFixedFields0(blockHeader);
-        uint8 have_new_producers;
-        (schedule, action_mroot, have_new_producers) = parseFixedFields1(blockHeader);
-
-        if(have_new_producers != 0 ) {
-            (version, amount, producerNames, producerKeyHighChunk) = parseNonFixedFields(blockHeader);
-        }
-    }
-
-    function verifyBlockSig(
-        bytes calldata blockHeader,             // from user
-        bytes32 blockMerkleHash,                // from user
-        bytes calldata pendingSchedule,         // from user 
-        uint8 sigV,                             // from user
-        bytes32 sigR,                           // from user
-        bytes32 sigS,                           // from user
-        bytes32[] calldata claimedSignerPubKey, // from user
-        bytes32 storedCompressedPubKey          // from storage (we maintain current schedule keys)
-    )
-        external
-        pure
-        returns (bool) 
-    {
-        bytes32 pairHash = sha256(abi.encodePacked(sha256(blockHeader), blockMerkleHash));
-        bytes32 pendingScheduleHash = sha256(pendingSchedule);
-        bytes32 finalHash = sha256(abi.encodePacked(pairHash, pendingScheduleHash));
-        address calcAddress = ecrecover(finalHash, sigV, sigR, sigS);
-        address claimedSignerAddress = address(
-            (uint)(keccak256(abi.encodePacked(claimedSignerPubKey[0], claimedSignerPubKey[1]))) & (2**(8*21)-1)
-        );
-
-        return (
-            (claimedSignerPubKey[0] == storedCompressedPubKey) && // signer is part of current schedule
-            (calcAddress == claimedSignerAddress)                 // signer signed the given block data 
-        );
-    }
-
-
-//////////////////////////////////////////////////
-
-    event VerifyData( bytes blockHeader,
-                      bytes32 blockMerkleHash,
-                      bytes32 pendingScheduleHash,
-                      uint8 sigV,
-                      bytes32 sigR,
-                      bytes32 sigS,
-                      bytes32 claimedSignerPubKeyFirst,
-                      bytes32 claimedSignerPubKeySecond,
-                      bool verified);
-
-    function verifyBlockSig2(
-        bytes memory blockHeader,
-        bytes32 blockMerkleHash,
-        bytes32 pendingScheduleHash,
-        uint8 sigV,
-        bytes32 sigR,
-        bytes32 sigS,
-        bytes32 claimedSignerPubKeyFirst,
-        bytes32 claimedSignerPubKeySecond
-    )
-        public
-        returns (bool) 
-    {
-        bytes32 pairHash = sha256(abi.encodePacked(sha256(blockHeader), blockMerkleHash));
-        bytes32 finalHash = sha256(abi.encodePacked(pairHash, pendingScheduleHash));
-        address calcAddress = ecrecover(finalHash, sigV, sigR, sigS);
-        address claimedSignerAddress = address(
-            (uint)(keccak256(abi.encodePacked(claimedSignerPubKeyFirst, claimedSignerPubKeySecond))) & (2**(8*21)-1)
-        );
-
-        emit VerifyData(blockHeader,
-                   blockMerkleHash,
-                   pendingScheduleHash,
-                   sigV,
-                   sigR,
-                   sigS,
-                   claimedSignerPubKeyFirst,
-                   claimedSignerPubKeySecond,
-                   calcAddress == claimedSignerAddress);
-
-        return ( calcAddress == claimedSignerAddress ); // signer signed the given block data
-    }
 
     // this is a temporary function. in the future the storing schedule will be validated.
     function storeSchedule(
@@ -221,70 +41,118 @@ contract Relay {
         bytes32[] memory blockMerkleHashs,
         bytes32[] memory blockMerklePaths,
         uint[] memory blockMerklePathSizes,
-        bytes32[] memory pendingScheduleHashes,
+        bytes32[] memory pendingScheduleHashs,
         uint8[15] memory sigVs,
         bytes32[15] memory sigRs,
         bytes32[15] memory sigSs,
         uint[15] memory claimedKeyIndices
     )
         public
-        returns (bool) {
+        view
+        returns (bool)
+    {
+        HeadersData memory headersData = HeadersData({
+            blockHeaders:blockHeaders,
+            blockHeaderSizes:blockHeaderSizes,
+            blockMerkleHashs:blockMerkleHashs,
+            blockMerklePaths:blockMerklePaths,
+            blockMerklePathSizes:blockMerklePathSizes,
+            pendingScheduleHashs:pendingScheduleHashs,
+            sigVs:sigVs,
+            sigRs:sigRs,
+            sigSs:sigSs,
+            claimedKeyIndices:claimedKeyIndices
+        });
 
-        uint offset_in_headers = 0; 
-        for (uint idx = 0; idx < blockHeaderSizes.length; idx++) {
-            bytes memory header = getOneHeader(blockHeaders, offset_in_headers, blockHeaderSizes[idx]);
-            offset_in_headers = offset_in_headers + blockHeaderSizes[idx];
+        return doVerifyBlockBasedOnSchedule(headersData);
+    }
 
-            bool verified = verifyBlockSig2(
-                header,
-                blockMerkleHashs[idx],
-                pendingScheduleHashes[idx],
-                sigVs[idx],
-                sigRs[idx],
-                sigSs[idx],
-                pubKeysFirstParts[claimedKeyIndices[idx]],
-                pubKeysSecondParts[claimedKeyIndices[idx]]
+    function doVerifyBlockBasedOnSchedule(HeadersData memory headersData) internal view returns (bool) {
+        uint offset_in_headers = 0;
+        uint pathOffset = 0;
+        bytes32 currentId;
+        bytes32 previousId = "";
+        for (uint idx = 0; idx < headersData.blockHeaderSizes.length; idx++) {
+            bytes memory header = getOneHeader(
+                headersData.blockHeaders,
+                offset_in_headers,
+                headersData.blockHeaderSizes[idx]
             );
-            if (!verified) {
-                return false;
+            offset_in_headers = offset_in_headers + headersData.blockHeaderSizes[idx];
+
+            bool valid = verifyBlockSig(
+                header,
+                headersData,
+                idx,
+                pubKeysFirstParts[headersData.claimedKeyIndices[idx]],
+                pubKeysSecondParts[headersData.claimedKeyIndices[idx]]
+            );
+            if (!valid) return false;
+
+            currentId = getIdFromHeader(header);
+            if(previousId != "") {
+                uint pathSize = headersData.blockMerklePathSizes[idx];
+                bytes32[] memory path = getOnePath(headersData.blockMerklePaths, pathOffset, pathSize);
+                pathOffset = pathOffset + pathSize;
+
+                valid = proofIsValid(previousId, path, headersData.blockMerkleHashs[idx]);
+                if (!valid) return false;
             }
+            previousId = currentId;
         }
+
         return true;
-
-
-        // for each 
-
-        // verify signed by a unique producer
-
-        // verify verify links to previous block
-
-        // return true;
     }
 
-    // tmp function for debug
-    function checkId(bytes memory header, uint32 blockNumber) public returns (bytes32) {
-        bytes memory result = sha256(header);
-        /*
-        block_id_type result = digest(); //fc::sha256::hash(*static_cast<const block_header*>(this));
-        result._hash[0] &= 0xffffffff00000000;
-        result._hash[0] += fc::endian_reverse_u32(block_num()); // store the block num in the ID, 160 bits is plenty for the hash
-        return result;
-        */
-        
+    function verifyBlockSig(
+        bytes memory blockHeader,
+        HeadersData memory headersData,
+        uint idx,
+        bytes32 claimedSignerPubKeyFirst,
+        bytes32 claimedSignerPubKeySecond
+    )
+        internal
+        pure
+        returns (bool) 
+    {
+        bytes32 pairHash = sha256(abi.encodePacked(sha256(blockHeader), headersData.blockMerkleHashs[idx]));
+        bytes32 finalHash = sha256(abi.encodePacked(pairHash, headersData.pendingScheduleHashs[idx]));
+        address calcAddress = ecrecover(finalHash, headersData.sigVs[idx], headersData.sigRs[idx], headersData.sigSs[idx]);
+        address claimedSignerAddress = address(
+            (uint)(keccak256(abi.encodePacked(claimedSignerPubKeyFirst, claimedSignerPubKeySecond))) & (2**(8*21)-1)
+        );
+
+        return ( calcAddress == claimedSignerAddress );
     }
 
+    function getId(bytes memory header, uint blockNum) internal pure returns (bytes32) {
+        bytes32 headerHash = sha256(header);
+        uint blockNumShifted = blockNum << (256 - 32);
+        uint mask = ((2**(256 - 32))-1);
+        uint result = ((uint)(headerHash) & mask) | blockNumShifted;
+        return (bytes32)(result);
+    }
 
+    function getBlockNumFromId(bytes32 id) internal pure returns (uint) {
+        return ((uint)(id) >> (256 - 32));
+    }
 
-//////////////////////////////////////////////////
+    function getIdFromHeader(bytes memory header) internal pure returns (bytes32) {
+        uint offset = TIMESTAMP_BYTES + PRODUCER_BYTES + CONFIRMED_BYTES;
+        uint previous = (uint256)(sliceBytes(header, offset, PREVIOUS_BYTES));
+        uint blockNum = getBlockNumFromId((bytes32)(previous)) + 1;
+        return getId(header, blockNum);
+    }
 
     function getOneHeader(
         bytes memory blockHeaders,
         uint offset_in_headers,
         uint headerSize
     )
-    internal
-    pure
-    returns (bytes memory) {
+        internal
+        pure
+        returns (bytes memory)
+    {
         bytes memory header = new bytes(headerSize);
         uint size = headerSize;
         uint offset_in_header = 0;
@@ -309,6 +177,22 @@ contract Relay {
         return header;
     }
 
+    function getOnePath(
+        bytes32[] memory blockMerklePaths,
+        uint pathOffset,
+        uint pathSize
+    )
+        internal
+        pure
+        returns (bytes32[] memory)
+    {
+        bytes32[] memory path = new bytes32[](pathSize);
+        for( uint i = 0; i < pathSize; i++) {
+            path[i] = blockMerklePaths[pathOffset + i];
+        }
+        return path;
+    }
+
     function makeCanonicalLeft(bytes32 self) internal pure returns (bytes32) {
         return (bytes32)((uint)(self) & AND_MASK);
     }
@@ -321,7 +205,7 @@ contract Relay {
         return (((uint)(self) >> 255) == 1);
     }
 
-    function proofIsValid(bytes32 leaf, bytes32[] calldata path, bytes32 expectedRoot) external pure returns (bool) {
+    function proofIsValid(bytes32 leaf, bytes32[] memory path, bytes32 expectedRoot) internal pure returns (bool) {
         bytes32 current = leaf;
         bytes32 left;
         bytes32 right;
--- test/relay.js
+++ test/relay.js
@@ -1,4 +1,3 @@
-const Helper = require("./helper.js")
 const bs58 = require('bs58')
 const ethUtil = require('ethereumjs-util')
 const keyUtils = require('eosjs-ecc/lib/key_utils.js');
@@ -53,61 +52,15 @@ const Relay = artifacts.require("Relay")
 
 contract("Relay", async accounts => {
 
-    it("verify block signature", async () => {
-        headerRaw   = "[ c6 fc 6e 47 00 00 00 00 00 ea 30 55 00 00 00 00 00 01 bc f2 f4 48 22 5d 09 96 85 f1 4d a7 68 03 02 89 26 af 04 d2 60 7e af cf 60 9c 26 5c 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 74 7d 10 3e 24 c9 6d eb 1b ee bc 13 eb 31 f7 c2 18 81 26 94 6c 86 77 df d1 69 1a f9 f9 c0 3a b1 00 00 00 00 00 00 ]"
-        bmRootRaw   = "[ 00 00 00 01 bc f2 f4 48 22 5d 09 96 85 f1 4d a7 68 03 02 89 26 af 04 d2 60 7e af cf 60 9c 26 5c ]"
-        scheduleRaw = "[ 00 00 00 00 01 00 00 00 00 00 ea 30 55 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf ]"  
-        signatureRaw = "SIG_K1_JuKvk5XSetfux7yvVZGyYkLMPTFyKAmg1HwikbKKUvNBkvtNWWHtWwRYoxYvY7csbXcMMSyiyiy4kvmgxrmW9UkeyPEfWr"
-        expectedSigningKeyRaw = "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"
-
-        header   = "0x"+toBuffer(headerRaw).toString("hex")
-        bmRoot   = "0x"+toBuffer(bmRootRaw).toString("hex")
-        schedule = "0x"+toBuffer(scheduleRaw).toString("hex")
-
-        let v,r,s
-        [v,r,s] = getSigParts(bs58SigToHex(signatureRaw))
-        expectedSigningKey = "0x"+bs58pubKeyToHex(expectedSigningKeyRaw).toString("hex")
-        // TODO: uncompress the key, for now using python code for it
-
-        // this is as calculated in uncompress.py and stripped of from leading 0x04
-        claimedSignerPubKey = [ expectedSigningKey, // "0xc0ded2bc1f1305fb0faac5e6c03ee3a1924234985427b6167ca569d13df435cf",
-                               "0xeeceff7130fd352c698d2279967e2397f045479940bb4e7fb178fd9212fca8c0"]
-        storedCompressedPubKey = expectedSigningKey
-
-        console.log("header", header)
-        console.log("bmRoot", bmRoot)
-        console.log("schedule", schedule)
-        console.log("v", v)
-        console.log("r", r)
-        console.log("s", s)
-        console.log("claimedSignerPubKey   ", claimedSignerPubKey)
-        console.log("storedCompressedPubKey", expectedSigningKeyRaw)
-
-        //04c0ded2bc1f1305fb0faac5e6c03ee3a1924234985427b6167ca569d13df435cfeeceff7130fd352c698d2279967e2397f045479940bb4e7fb178fd9212fca8c0
-
-        const relay = await Relay.new()
-        const verified = await relay.verifyBlockSig(
-                header,
-                bmRoot,
-                schedule,
-                v,
-                r,
-                s,
-                claimedSignerPubKey,
-                storedCompressedPubKey
-        )
-        console.log(verified)
-        assert(verified, "block not verified correctly")
-    })
-
     it("parse header", async () => {
         // this includes new producers:
         headerRaw = "[ 95 18 79 47 00 00 00 00 00 ea 30 55 00 00 00 00 00 01 bc f2 f4 48 22 5d 09 96 85 f1 4d a7 68 03 02 89 26 af 04 d2 60 7e af cf 60 9c 26 5c 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 74 7d 10 3e 24 c9 6d eb 1b ee bc 13 eb 31 f7 c2 18 81 26 94 6c 86 77 df d1 69 1a f9 f9 c0 3a b1 00 00 00 00 01 57 01 00 00 04 00 00 00 00 00 ea 30 55 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 00 00 00 00 73 a2 c9 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 00 00 00 80 49 af f1 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 00 00 00 80 69 a2 73 00 02 c0 de d2 bc 1f 13 05 fb 0f aa c5 e6 c0 3e e3 a1 92 42 34 98 54 27 b6 16 7c a5 69 d1 3d f4 35 cf 00 ]"
         header = "0x" + toBuffer(headerRaw).toString("hex")
-        console.log("header", header)
+        /* console.log("header", header) */
 
         const relay = await Relay.new()
         const result = await relay.parseHeader(header);
+        /*
         console.log("timestamp: ", result[0]);
         console.log("producer: ", result[1]);
         console.log("confirmed: ", result[2]);
@@ -119,49 +72,7 @@ contract("Relay", async accounts => {
         console.log("amount: ", result[8]);
         console.log("producerNames: \n", result[9]);
         console.log("producerKeysHigh: \n", result[10]);
-    });
-
-    it("validate merkle proof", async () => {
-        
-        leaves = ["0x0ff38b06e8d91eb9e0e315baadf124dd6088d985830af83e5fe9d19a1aec94db",
-                  "0x04ca2c2c485ebd1ff324b61ff351cf8810d40fb53cc54f1de4136461b4d15a91",
-                  "0xf642ce52f6d266daeffda3798cde463b375515e2ff07f28169142cda4fae81da",
-                  "0xa1c526efa754b4c7b86bea5096ced3da383fb95c2f06b5147843b818055e5926",
-                  "0x3eaa2d991b2e6bed7b11cdd1ecc4f591fdf32102c4b17417b71c255edd2116df",
-                  "0x1863f557d6c65a9bb14c681dac086acde422725f2e51490d212fd60f15c0e457",
-                  "0x64f457ce04f13bbf1f1f3fd108ba43ccc6e77f6851029b438bcf436be412308a"]
-
-         paths = [["0x84ca2c2c485ebd1ff324b61ff351cf8810d40fb53cc54f1de4136461b4d15a91",
-                  "0x98a5352460276f18cb130dc10c2266f98b4f98844fb06a8cb8d156a23abb36de",
-                  "0xbe99e8ffbfd7eaf2969c487ce05c61d106cf87f1b1db50a88c1ff0339ba29040"],
-                 ["0x0ff38b06e8d91eb9e0e315baadf124dd6088d985830af83e5fe9d19a1aec94db",
-                  "0x98a5352460276f18cb130dc10c2266f98b4f98844fb06a8cb8d156a23abb36de",
-                  "0xbe99e8ffbfd7eaf2969c487ce05c61d106cf87f1b1db50a88c1ff0339ba29040"],
-                 ["0xa1c526efa754b4c7b86bea5096ced3da383fb95c2f06b5147843b818055e5926",
-                  "0x6b104f5d1fd3a7efc6ded56f337f4e0dc1b7b67a88da128613d43c221d0e6718",
-                  "0xbe99e8ffbfd7eaf2969c487ce05c61d106cf87f1b1db50a88c1ff0339ba29040"],
-                 ["0x7642ce52f6d266daeffda3798cde463b375515e2ff07f28169142cda4fae81da",
-                  "0x6b104f5d1fd3a7efc6ded56f337f4e0dc1b7b67a88da128613d43c221d0e6718",
-                  "0xbe99e8ffbfd7eaf2969c487ce05c61d106cf87f1b1db50a88c1ff0339ba29040"],
-                 ["0x9863f557d6c65a9bb14c681dac086acde422725f2e51490d212fd60f15c0e457",
-                  "0xcfdf4879f658aa25845a1058400194db668177c07367eda6239d0330559a62bc",
-                  "0x503dca34c439c417f755cde8420379eafb9b2d7179b5acfc1b302e5fc869d975"],
-                 ["0x3eaa2d991b2e6bed7b11cdd1ecc4f591fdf32102c4b17417b71c255edd2116df",
-                  "0xcfdf4879f658aa25845a1058400194db668177c07367eda6239d0330559a62bc",
-                  "0x503dca34c439c417f755cde8420379eafb9b2d7179b5acfc1b302e5fc869d975"],
-                 ["0xe4f457ce04f13bbf1f1f3fd108ba43ccc6e77f6851029b438bcf436be412308a",
-                  "0x099df29b96e436d09301bda47f9ebc055b5e91bc24a10a614c39bc4e2b213a86",
-                  "0x503dca34c439c417f755cde8420379eafb9b2d7179b5acfc1b302e5fc869d975"]]
-
-        root = "0xdeac5a4438f924f5c24fc7c65bf7d0e39817db67e958ce81772b05a072e2e1fd";
-
-        const relay = await Relay.new()
-        var i;
-        for (i = 0; i < leaves.length; i++) {
-            const valid = await relay.proofIsValid(leaves[i], paths[i], root);
-            console.log(valid);
-            assert(valid, "proof is not valid")
-        }
+        */
     });
 
     it("parse 15 blocks", async () => {
@@ -178,6 +89,7 @@ contract("Relay", async accounts => {
             publicKeysForPython.push((bs58.decode(expectedSigningKey.slice(3)).toString("hex")).slice(0,-8))
             namesToIdx[thisData.producer_name] = j
         }
+
         // using the publicKeysForPython and uncompress.py script uncompressed_keys.json have been created 
         // remove if need to create input for python - console.log("publicKeysForPython", publicKeysForPython)
 
@@ -231,6 +143,4 @@ contract("Relay", async accounts => {
          assert(valid);
 
     });
-    
-    
 })
